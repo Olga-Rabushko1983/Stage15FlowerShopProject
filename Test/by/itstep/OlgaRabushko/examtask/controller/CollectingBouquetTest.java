3288d73859726a49a922322b7fc31f2d3f8df1e9
Olga-Rabushko1983 <107210895+Olga-Rabushko1983@users.noreply.github.com>
2022-07-21 02:07:48 +0300

    Update CollectingBouquetTest.java

--- Test/by/itstep/OlgaRabushko/examtask/controller/CollectingBouquetTest.java
+++ Test/by/itstep/OlgaRabushko/examtask/controller/CollectingBouquetTest.java
@@ -1,7 +1,31 @@
 package by.itstep.OlgaRabushko.examtask.controller;
 
+import by.itstep.OlgaRabushko.examtask.model.CollectingBouquet;
+import by.itstep.OlgaRabushko.examtask.model.Flower;
+import by.itstep.OlgaRabushko.examtask.model.Peony;
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class CollectingBouquetTest extends TestCase {
+import static org.junit.Assert.*;
 
+//public class CollectingBouquetTest {
+
+
+ @Test
+    public void testMaxCollectingBouquetPositive() {
+        Flower[] flowers = {5, 15, 10};
+        int expected = 15;
+        int actual = CollectingBouquet.max(flowers);
+        Assert.assertEquals(expected, actual, 0.001);
+    }
+
+    @Test
+    public void testMinCollectingBouquetPositive() {
+        Flower[] flowers = {5, 15, 10};
+       
+        int expected = 5;
+        int actual = CollectingBouquet.min(flowers);
+        Assert.assertEquals(expected, actual, 0.001);
+    }
 }
